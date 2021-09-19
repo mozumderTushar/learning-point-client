7158ef5dfd2c5a2e47383a6ef632646d8b8d9258
mozumderTushar <itztushar55@gmail.com>
2021-09-19 15:06:05 +0600

    nav & landing added

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigation from '../../Shared/Navigation/Navigation';
+import Landing from '../Landing/Landing';
+
+const Home = () => {
+  return (
+    <div>
+      <Navigation />
+      <Landing />
+    </div>
+  );
+};
+
+export default Home;
