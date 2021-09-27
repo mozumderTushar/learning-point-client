d782f613587b90872f1ded72cd995329261633aa
mozumderTushar <itztushar55@gmail.com>
2021-09-27 23:21:47 +0600

    teacher signup api added

--- src/components/TeacherSignup/TeacherSignUp.js
+++ src/components/TeacherSignup/TeacherSignUp.js
@@ -11,112 +11,143 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Navigation from '../Shared/Navigation/Navigation';
+import { useHistory, useLocation } from 'react-router-dom';
 import './TeacherSignup.css';
 
 const theme = createTheme();
 
 const TeacherSignup = () => {
+  const history = useHistory();
   const handleSubmit = (e) => {
     e.preventDefault();
     const data = new FormData(e.currentTarget);
-    console.log({
-      firstName: data.get('firstName'),
-      lastName: data.get('lastName'),
-      email: data.get('email'),
-      qualification: data.get('qualification'),
-      subject: data.get('subject'),
-      charge: data.get('charge'),
-      time: data.get('time'),
-      contact: data.get('contact'),
-      password: data.get('password'),
-    });
+
+    const firstName = data.get('firstName');
+    const lastName = data.get('lastName');
+    const email = data.get('email');
+    const qualification = data.get('qualification');
+    const subject = data.get('subject');
+    const charge = data.get('charge');
+    const time = data.get('time');
+    const contact = data.get('contact');
+    const password = data.get('password');
+
+    const user = {
+      firstName, lastName, email, password, qualification, subject, charge, time, contact
+    }
+
+    console.log(user);
+    fetch('http://localhost:2000/api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(response => response.json())
+      .then(data => {
+        alert('Success');
+        history.push('/');
+
+        if (data.status === 200) {
+          alert('Success');
+          history.push('/');
+
+        } else {
+          if (data.status === 400) {
+            console.log(data.error);
+          }
+        }
+      })
+
     e.target.reset();
   };
-  return (
-    <div className="container-teacher">
-      <Navigation />
-      <ThemeProvider theme={theme}>
-        <Container component="main" maxWidth="l">
-          <CssBaseline />
-          <Box
-            sx={{
-              marginTop: 8,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: '#1dbf73' }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              Teacher Sign up
-            </Typography>
-            <Box className="ml-md-5 mr-md-5" component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-              <Grid container spacing={2}>
-                <Grid item xs={12} sm={4}>
-                  <TextField
-                    autoComplete="fname"
-                    name="firstName"
-                    required
-                    fullWidth
-                    id="firstName"
-                    label="First Name"
-                    autoFocus
-                  />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="lastName"
-                    label="Last Name"
-                    name="lastName"
-                    autoComplete="lname"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={4}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="email"
-                    label="Email Address"
-                    name="email"
-                    autoComplete="email"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="qualification"
-                    label="Qualification"
-                    name="qualification"
-                    autoComplete="qualification"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="subject"
-                    label="Which Subject do you want to teach?"
-                    name="subject"
-                    autoComplete="subject"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="charge"
-                    label="Charge Per Lecture(in Taka)"
-                    name="charge"
-                    autoComplete="charge"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
+
+return (
+  <div className="container-teacher">
+    <Navigation />
+    <ThemeProvider theme={ theme }>
+      <Container component="main" maxWidth="l">
+        <CssBaseline />
+        <Box
+          sx={ {
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          } }
+        >
+          <Avatar sx={ { m: 1, bgcolor: '#1dbf73' } }>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Teacher Sign up
+          </Typography>
+          <Box className="ml-md-5 mr-md-5" component="form" noValidate onSubmit={ handleSubmit } sx={ { mt: 3 } }>
+            <Grid container spacing={ 2 }>
+              <Grid item xs={ 12 } sm={ 4 }>
+                <TextField
+                  autoComplete="fname"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={ 12 } sm={ 4 }>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                />
+              </Grid>
+              <Grid item xs={ 12 } sm={ 4 }>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={ 12 } sm={ 6 }>
+                <TextField
+                  required
+                  fullWidth
+                  id="qualification"
+                  label="Qualification"
+                  name="qualification"
+                  autoComplete="qualification"
+                />
+              </Grid>
+              <Grid item xs={ 12 } sm={ 6 }>
+                <TextField
+                  required
+                  fullWidth
+                  id="subject"
+                  label="Which Subject do you want to teach?"
+                  name="subject"
+                  autoComplete="subject"
+                />
+              </Grid>
+              <Grid item xs={ 12 } sm={ 6 }>
+                <TextField
+                  required
+                  fullWidth
+                  id="charge"
+                  label="Charge Per Lecture(in Taka)"
+                  name="charge"
+                  autoComplete="charge"
+                />
+              </Grid>
+              <Grid item xs={ 12 } sm={ 6 }>
+                <TextField
                   required
                   fullWidth
                   id="time"
@@ -124,52 +155,52 @@ const TeacherSignup = () => {
                   defaultValue="10:00"
                   name="time"
                   type="time"
-                  autoComplete="time"  
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    id="contact"
-                    label="Contact Number"
-                    name="contact"
-                    autoComplete="contact"
-                  />
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                  <TextField
-                    required
-                    fullWidth
-                    name="password"
-                    label="Password"
-                    type="password"
-                    id="password"
-                    autoComplete="new-password"
-                  />
-                </Grid>
+                  autoComplete="time"
+                />
               </Grid>
-              <Button
-                style={{margin: '30px auto 10px', display: "flex", width: " 300px"}}
-                type="submit"
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign Up
-              </Button>
-              <Grid container justifyContent="center">
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    Already have an account? Sign in
-                  </Link>
-                </Grid>
+              <Grid item xs={ 12 } sm={ 6 }>
+                <TextField
+                  required
+                  fullWidth
+                  id="contact"
+                  label="Contact Number"
+                  name="contact"
+                  autoComplete="contact"
+                />
               </Grid>
-            </Box>
+              <Grid item xs={ 12 } sm={ 6 }>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              style={ { margin: '30px auto 10px', display: "flex", width: " 300px" } }
+              type="submit"
+              variant="contained"
+              sx={ { mt: 3, mb: 2 } }
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="center">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
           </Box>
-        </Container>
-      </ThemeProvider>
-    </div>
-  );
+        </Box>
+      </Container>
+    </ThemeProvider>
+  </div>
+);
 };
 
 export default TeacherSignup;
